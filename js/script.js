1f50234fe6302d500552b7183bcedc6a53fe404e
nazmul hasan <nhsohel01@gmail.com>
2020-04-02 11:07:52 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,93 @@
+
+
+
+// OUR PORTFOLIO PART START================
+// external js: isotope.pkgd.js
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '[data-category]',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
+  });
+  
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+  
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  // bind sort button click
+  $('#sorts').on( 'click', 'button', function() {
+    var sortByValue = $(this).attr('data-sort-by');
+    $grid.isotope({ sortBy: sortByValue });
+  });
+  
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+// OUR PORTFOLIO PART END================
+
+// FOR COUNTER UP PART START================
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+// FOR COUNTER UP PART END================
+
+// GOOGLE MAP START======================
+
+$(document).on('scroll', function(e){
+  /* apears to 30% of total page ;) */
+  if(($(document).scrollTop() * 500 / $(document).height()) > 30){
+    $(".scrollup").show();
+  }
+  else{
+    $(".scrollup").hide();
+  }
+});
+$(".scrollup").click(function(){
+var body = $("html, body");
+body.animate({scrollTop:0}, '600',);
+});
+
+
+
+// GOOGLE MAP END======================
+
+
+
+
+
